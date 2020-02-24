e503bbe42f2300ba4cd26857383498954252f744
lidf077 <lidf077@gmail.com>
2020-02-24 23:04:04 +0800

    单例模式测试

--- src/main/java/com/dongfang/advanced/designpatterns/creational/singleton/LazySingleton.java
+++ src/main/java/com/dongfang/advanced/designpatterns/creational/singleton/LazySingleton.java
@@ -1,9 +1,14 @@
 package com.dongfang.advanced.designpatterns.creational.singleton;
 
-public class LazySingleton {
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+public class LazySingleton implements Serializable {
     private static LazySingleton instance = null; // 静态变量必须初始化
 
     private LazySingleton() {
+        // 这样如果先通过反射调用，还是有漏洞的
+        if (instance != null) throw new RuntimeException();
     }
 
     // 整个方法都被同步了，效率较低
@@ -11,6 +16,11 @@ public class LazySingleton {
         if (instance == null) {
             instance = new LazySingleton();
         }
+        return instance;
+    }
+
+    // 反序列化，如果定义了这个方法，直接返回此方法指定的对象，而不是创建新的对象
+    private Object readResolve() throws ObjectStreamException {
         return instance;
     }
 }
--- src/main/java/com/dongfang/advanced/designpatterns/creational/singleton/SingletonDemo.java
+++ src/main/java/com/dongfang/advanced/designpatterns/creational/singleton/SingletonDemo.java
@@ -2,6 +2,12 @@ package com.dongfang.advanced.designpatterns.creational.singleton;
 
 import org.junit.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class SingletonDemo {
     @Test
     public void testStaticSingleton() {
@@ -18,5 +24,40 @@ public class SingletonDemo {
     @Test
     public void testEnumSingleton() {
         System.out.println("EnumSingleton.INSTANCE = " + EnumSingleton.INSTANCE.ordinal());
+    }
+
+    /**
+     * 反射和反序列化破解单例
+     */
+    @Test
+    public void testReflectSingleton() throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class<?> aClass = Class.forName("com.dongfang.advanced.designpatterns.creational.singleton.LazySingleton");
+
+        Constructor<?> constructor = aClass.getDeclaredConstructor(null);
+        constructor.setAccessible(true);
+        LazySingleton instance = (LazySingleton) constructor.newInstance();
+        System.out.println("instance = " + instance);
+
+
+        System.out.println("LazySingleton.getInstance() = " + LazySingleton.getInstance());
+
+    }
+
+    @Test
+    public void testSerializable() throws IOException, ClassNotFoundException {
+        LazySingleton instance = LazySingleton.getInstance();
+//        FileOutputStream fos = new FileOutputStream("D:\\lidf\\entrance\\object.txt");
+//        ObjectOutputStream oos = new ObjectOutputStream(fos);
+//        oos.writeObject(instance);
+//        oos.close();
+//        fos.close();
+
+        FileInputStream fis = new FileInputStream("D:\\lidf\\entrance\\object.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        LazySingleton fileObject = (LazySingleton) ois.readObject();
+
+        // 反序列化破解单例
+        System.out.println("(instance == fileObject) = " + (instance == fileObject));
+
     }
 }
