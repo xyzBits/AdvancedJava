56a25472ec7b751278831e906bce93a5f3670ea9
lidf077 <lidf077@gmail.com>
2020-03-06 21:51:49 +0800

    jvm的栈内存结构

---
+++ src/main/java/com/dongfang/advanced/jvm/memorystructure/线程死锁.java
@@ -0,0 +1,40 @@
+package com.dongfang.advanced.jvm.memorystructure;
+
+/**
+ * 演示线程死锁
+ */
+class A {
+};
+
+class B {
+};
+
+public class 线程死锁 {
+    static A a = new A();
+    static B b = new B();
+
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            synchronized (a) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                synchronized (b) {
+                    System.out.println("我获得了 a 和 b");
+                }
+            }
+        }).start();
+        Thread.sleep(1000);
+        new Thread(() -> {
+            synchronized (b) {
+                synchronized (a) {
+                    System.out.println("我获得了 a 和 b");
+                }
+            }
+        }).start();
+    }
+
+}
